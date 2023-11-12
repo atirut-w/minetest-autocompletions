931a3cb1b8bb77ab72cb338ef8e16c17fddd232a
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-11-12 22:37:13 +0700

    Add description to hotbar-related functions

--- classes/objectref.lua
+++ classes/objectref.lua
@@ -4,20 +4,26 @@ local ObjectRef = {}
 
 -- Player-only functions
 
+--- Sets number of items in built-in hotbar.
 ---@param count number
 function ObjectRef.hud_set_hotbar_itemcount(self, count) end
 
+--- Gets number of items in built-in hotbar.
 ---@return number
 function ObjectRef.hud_get_hotbar_itemcount(self) end
 
+--- Sets image of built-in hotbar.
 ---@param texture string
 function ObjectRef.hud_set_hotbar_image(self, texture) end
 
+--- Gets image of built-in hotbar.
 ---@return string
 function ObjectRef.hud_get_hotbar_image(self) end
 
+--- Sets image of built-in hotbar selection.
 ---@param texture string
 function ObjectRef.hud_set_hotbar_selected_image(self, texture) end
 
+--- Gets image of built-in hotbar selection.
 ---@return string
 function ObjectRef.hud_get_hotbar_selected_image(self) end
