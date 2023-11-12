93bc35d5c3cbcb5935ea80b7dc6ba206c0240a5e
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-11-12 21:37:33 +0700

    Define aliasing functions

--- registration/environment.lua
+++ registration/environment.lua
@@ -3,3 +3,11 @@
 ---@param name string
 ---@param def NodeDefinition
 function minetest.register_node(name, def) end
+
+---@param alias string
+---@param original string
+function minetest.register_alias(alias, original) end
+
+---@param alias string
+---@param original string
+function minetest.register_alias_force(alias, original) end
