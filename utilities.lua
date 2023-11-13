b573e04e3274590505b3a29d0a10580d96e42bb4
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-11-13 22:06:32 +0700

    Define some utility functions

---
+++ utilities.lua
@@ -0,0 +1,10 @@
+---@meta
+
+--- Get the name of the currently loading mod.
+---@return string
+function minetest.get_current_modname() end
+
+--- Get the path to a mod's directory.
+---@param modname string
+---@return string|nil
+function minetest.get_modpath(modname) end
